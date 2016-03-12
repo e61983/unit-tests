8edeed0b8801e19534ab23f7a699806f428535f0
Hua-Yuan <e61983+git@gmail.com>
2016-03-13 06:26:56 +0800

    using test marco of unit test

--- test-swap.sh
+++ test-swap.sh
@@ -5,6 +5,7 @@ echo r >> _tmp.gdb
 echo source scripts/create_list.gdb >> _tmp.gdb
 echo source scripts/printf_list.gdb >> _tmp.gdb
 echo source scripts/find_node_address.gdb >> _tmp.gdb
+echo source scripts/test.gdb >> _tmp.gdb
 
 echo 'set $head = (pList)0' >> _tmp.gdb
 echo 'set $node1 = (pList)0' >> _tmp.gdb
@@ -17,7 +18,7 @@ echo 'create_list $head' $var >> _tmp.gdb
 exec < $1
 echo ' '>$2
 echo 'set logging file '$2 >> _tmp.gdb
-
+echo 'set $RESULT = 0' >> _tmp.gdb
 while (read var) 
 do
 	read var
@@ -39,8 +40,17 @@ do
 	echo 'p "new_list"' >> _tmp.gdb
 	echo 'printf_list $head' >> _tmp.gdb
 	echo 'set logging off' >> _tmp.gdb
+   echo 'set logging on' >> _tmp.gdb
+   echo 'p "change '$node_1'  '$node_2'"' >> _tmp.gdb
+   echo 'p "new_list"' >> _tmp.gdb
+   echo 'printf_list $head' >> _tmp.gdb
+   echo 'p "test '$node_1' '$node_2'"' >> _tmp.gdb
+   echo 'test $old_node1_next $node2' >>_tmp.gdb
+   echo 'p "test '$node_2' '$node_1'"' >> _tmp.gdb
+   echo 'test $old_node2_next $node1' >>_tmp.gdb
+   echo 'set logging off' >> _tmp.gdb
 done
-echo 'q' >> _tmp.gdb
+echo 'q $RESULT' >> _tmp.gdb
 echo 'y' >> _tmp.gdb
 
 gdb -q -x _tmp.gdb bin-swap >> /dev/null
