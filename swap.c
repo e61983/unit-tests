e5af24dc0b86f48a6b1de94c8f6461311cebe9e2
Hua-Yuan <e61983+git@gmail.com>
2016-03-17 05:31:42 +0800

    remove unused code and restruct the working flow
    setp 1. find the pre node
    setp 2. this is case: swap with head ?
    setp 3. can find the pre node of node 1 and node 2 ?
    setp 4. do stand swap

--- swap.c
+++ swap.c
@@ -6,76 +6,47 @@ pList swap(pList head, pList node_1, pList node_2)
             (node_1 == node_2))
         return head;
 
-    int num_pre_node_1_and_node_2 = 0;
-
-    pList _head = head;
     pList pre_node_1 = NULL;
     pList pre_node_2 = NULL;
-    pList tmp_node = NULL;
+    pList root = head;
 
-    while (head && head->next) {
+    /* find pre_node */
+    for (; head && head->next && head->next != root ; head = head->next) {
         if (head->next == node_1) {
             pre_node_1 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
         }
 
         if (head->next == node_2) {
             pre_node_2 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
         }
-        head = head->next;
     }
 
-    head = _head;
-    if (head == node_1) {
-        pre_node_1 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+    /* case: swap with head  */
+    if( node_1 == root || node_2 ==root) {
+        pList first_node = node_1 == root ? node_1:node_2;
+        pList second_node = node_1 == root ? node_2:node_1;
+        pList pre_node = node_1 == root ? pre_node_2:pre_node_1;
+
+        if(!pre_node)
+            return root;
+
+        pre_node->next = first_node;
+        pList tmp_node = first_node->next;
+        first_node->next=second_node->next;
+        second_node->next=tmp_node;
+        return second_node;
     }
 
-    if (head == node_2) {
-        pre_node_2 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
-    }
+    /* node1 or node2 can't find pre node  */
+    if(!(pre_node_1 && pre_node_2))
+        return root ;
 
-    if (num_pre_node_1_and_node_2 != 2)
-        return head;
-
-    if (pre_node_1 == NULL) {
-        pre_node_2->next = node_1;
-        tmp_node = node_1->next;
-        node_1->next = node_2->next;
-        node_2->next = tmp_node;
-        return node_2;
-    }
-
-    if (pre_node_2 == NULL) {
-        pre_node_1->next = node_2;
-        tmp_node = node_2->next;
-        node_2->next = node_1->next;
-        node_1->next = tmp_node;
-        return node_1;
-    }
-
-    if (node_2->next == node_1) {
-        pre_node_2->next = node_1;
-        tmp_node = node_1->next;
-        node_1->next = node_2;
-        node_2->next = tmp_node;
-        return head;
-    }
-
-    if (node_1->next == node_2) {
-        pre_node_1->next = node_2;
-        tmp_node = node_2->next;
-        node_2->next = node_1;
-        node_1->next = tmp_node;
-        return head;
-    }
-
+    /* swap */
     pre_node_1->next = node_2;
-    tmp_node = node_2->next;
-    node_2->next = node_1->next;
     pre_node_2->next = node_1;
-    node_1->next = tmp_node;
-    return head;
+
+    pList tmp_node = node_1->next;
+    node_1->next = node_2->next;
+    node_2->next = tmp_node;
+    return root;
 }
